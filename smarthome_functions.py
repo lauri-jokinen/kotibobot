203c7f48f34c077ad73af5c59a79b1651c7bd316
Lauri Jokinen <lateus96@gmail.com>
2021-05-04 12:39:58 +0300

    Some functions added

--- smarthome_functions.py
+++ smarthome_functions.py
@@ -2,41 +2,41 @@
 import json
 import subprocess # ubuntu bash
 
-with open("/home/lowpaw/Downloads/telegram-koodeja.json") as json_file:
+with open(”/home/lowpaw/Downloads/telegram-koodeja.json”) as json_file:
     koodit = json.load(json_file)
 
-rooms = json.loads("{}")
+rooms = json.loads(”{}”)
 
-rooms["olkkari"] = json.loads("{}")
-rooms["olkkari"]["eq-3"] = [koodit["eq-3-1"], koodit["eq-3-2"]]
+rooms[”olkkari”] = json.loads(”{}”)
+rooms[”olkkari”][”eq-3”] = [koodit[”eq-3-1”], koodit[”eq-3-2”]]
 
-rooms["makkari"] = json.loads("{}")
-rooms["makkari"]["eq-3"] = [koodit["eq-3-3"]]
+rooms[”makkari”] = json.loads(”{}”)
+rooms[”makkari”][”eq-3”] = [koodit[”eq-3-3”]]
 
-rooms["työkkäri"] = json.loads("{}")
-rooms["työkkäri"]["eq-3"] = [koodit["eq-3-4"]]
+rooms[”työkkäri”] = json.loads(”{}”)
+rooms[”työkkäri”][”eq-3”] = [koodit[”eq-3-4”]]
 
 # Create an array with all eq-3 devices
 eq3s = []
 for room in rooms:
-  if "eq-3" in rooms[room]
-    for eq3 in rooms[room]["eq-3"]:
+  if ”eq-3” in rooms[room]
+    for eq3 in rooms[room][”eq-3”]:
       eq3s.append(eq3)
 
 def mac_to_room(mac):
   for room in rooms:
-    if "eq-3" in rooms[room] and mac in rooms[room]["eq-3"]
+    if ”eq-3” in rooms[room] and mac in rooms[room][”eq-3”]
       return room
-  return "!room-undefined!"
+  return ”!room-undefined!”
 
 def eq3_command(str):
-  res = subprocess.run(['./eq3.exp', str], stdout=subprocess.PIPE)
-  return res.stdout.decode('utf-8')
+  res = subprocess.run([’./eq3.exp’, str], stdout=subprocess.PIPE)
+  return res.stdout.decode(’utf-8’)
 
 def bad_battery_eq3():
   low_battery = []
   for eq3 in eq3s:
-    if "battery" in eq3_command(eq3 + " sync"): # or "low battery" ?
+    if ”battery” in eq3_command(eq3 + ” sync”): # or ”low battery” ?
       low_battery.append(mac_to_room(eq3))
   return low_battery
 
@@ -44,7 +44,7 @@ def bad_battery_eq3():
 def check_connections_eq3():
   bad_connection = []
   for eq3 in eq3s:
-    # if eq3_command(eq3 + " status") == "ok":
+    # if eq3_command(eq3 + ” status”) == ”ok”:
     if false:
     bad_connection.append(mac_to_room(eq3))
   return bad_connection
@@ -52,9 +52,23 @@ def check_connections_eq3():
 def sys_check():
   x = check_connections_eq3()
   if x != []:
-    return "Yhteyttä ei saatu muodostettua huoneen (tai huoneiden) " + ", ".join(x) + " eq-3 termostaatteihin."
+    return ”Yhteyttä ei saatu muodostettua huoneen (tai huoneiden) ” + ”, ”.join(x) + ” eq-3 termostaatteihin.”
   x = bad_batteries()
   if x != []:
-    return "Huoneen (tai huoneiden) " + ", ".join(x) + " eq-3 termostaateissa on patterit lopussa."
+    return ”Huoneen (tai huoneiden) ” + ”, ”.join(x) + ” eq-3 termostaateissa on patterit lopussa.”
   sync_time()
   # check that rooms have same settings
+
+def human_to_machine(str):
+  selected_rooms = str.split(" ")[0].split("-")
+  command = " " + (" ").join(str.split(" ")[1:])
+  if selected_rooms == "kaikki":
+    for eq3 in eq3s:
+      eq3_command(eq3 + command)
+  else:
+    for room in selected_rooms:
+      if room in rooms and "eq-3" in rooms[room]:
+        for eq3 in rooms[room]["eq-3"]:
+          eq3_command(eq3 + command)
+      else:
+        return "Room '" + room + "' does not exist. Rooms are " + ", ".join(rooms) + ". I'll continue without it."
