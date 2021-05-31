ef4b4c6769149471361c257124948d580b6f4612
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2021-05-31 13:28:22 +0300

    renamed, cleaned and commented

--- kotibobot_functions.py
+++ kotibobot_functions.py
@@ -9,6 +9,8 @@ with open("/home/lowpaw/Downloads/telegram-koodeja.json") as json_file:
 
 rooms = json.loads("{}")
 
+# FILL IN THE ROOM INFORMATION IN JSON FORMAT
+# {"room1" : {"eq3": ["mac1","mac2"], "mi" : ["mac3"]}, ...}
 rooms["olkkari"] = json.loads("{}")
 rooms["olkkari"]["eq3"] = [koodit["keittiön nuppi"], koodit["olkkarin nuppi"]]
 rooms["olkkari"]["mi"] = [koodit["olkkarin lämpömittari"]]
@@ -21,7 +23,8 @@ rooms["työkkäri"] = json.loads("{}")
 rooms["työkkäri"]["eq3"] = [koodit["työkkärin nuppi"]]
 rooms["työkkäri"]["mi"] = [koodit["työkkärin lämpömittari"]]
 
-
+# FILL IN THE MAC ADDRESSES AND NAMES OF ALL DEVICES
+# {"mac1": "device name", ...}
 mac_to_name = json.loads("{}")
 mac_to_name[koodit["keittiön nuppi"]] = "keittiön nuppi"
 mac_to_name[koodit["olkkarin nuppi"]] = "olkkarin nuppi"
@@ -31,20 +34,21 @@ mac_to_name[koodit["työkkärin lämpömittari"]] = "työkkärin lämpömittari"
 mac_to_name[koodit["makkarin lämpömittari"]] = "makkarin lämpömittari"
 mac_to_name[koodit["olkkarin lämpömittari"]] = "olkkarin lämpömittari"
 
-# kokeillaan gittiä :)
+# Next we construct all sorts of structs and arrays ...
 
+# name to mac, inverse of the above
 name_to_mac = json.loads("{}")
 for mac in mac_to_name:
   name_to_mac[mac_to_name[mac]] = mac
 
-# Create an array with all eq-3 devices (MAC)
+# Create an array with all mac-addresses of all the eq-3 devices
 eq3s = []
 for room in rooms:
   if "eq3" in rooms[room]:
     for eq3 in rooms[room]["eq3"]:
       eq3s.append(eq3)
 
-# Create an array with all mi devices (MAC)
+# Create an array with all mac-addresses of all the mi devices
 mis = []
 for room in rooms:
   if "mi" in rooms[room]:
@@ -52,6 +56,7 @@ for room in rooms:
       mis.append(mi)
 
 # eq3s in rooms
+# {"room1" : ["mac1"], ...}
 eq3_in_rooms = json.loads("{}")
 for room in rooms:
   eq3_in_rooms[room] = []
@@ -59,8 +64,8 @@ for room in rooms:
     for eq3 in rooms[room]["eq3"]:
       eq3_in_rooms[room].append(mac_to_name[eq3])
 
-
 # mi in rooms
+# {"room1" : ["mac1"], ...}
 mi_in_rooms = json.loads("{}")
 for room in rooms:
   mi_in_rooms[room] = []
@@ -91,27 +96,26 @@ def eq3_command(str):
       res_str = res.stdout.decode('utf-8')
   return res_str
 
-#def bad_battery_eq3s():
-#  low_battery = []
-#  for eq3 in eq3s:
-#    if "low battery" in eq3_command(eq3 + " sync"):
-#      low_battery.append(mac_to_room(eq3))
-#  return low_battery
+# Extra definitions to the basic commands
+def eq3_command_with_extras(str):
+  if in_allday_timer_format(str):
+    return eq3_command(str + ' 24:00')
+  else:
+    return eq3_command(str)
 
 # input: makkari offset -1
 #        työkkäri-makkari timer 12:00-13:00 17
-def human_to_machine(str):
+def eq3_command_human(str):
   str = remove_extra_spaces(str)
   selected_rooms = str.split(" ")[0].split("-")
-  print(str)
+  
   # No command -> return status
   if len(str.split(" ")) == 1:
     command = " status"
   else:
     command = " " + (" ").join(str.split(" ")[1:])
-  # TODO: jaa osiin, alla vain vakiokomennot
   
-  res = []
+  res = [] # resulting strings are collected to this array
   if selected_rooms[0] == "kaikki":
     for eq3 in eq3s:
       res.append(replace_macs_to_names(eq3_command_with_extras(eq3 + command)))
@@ -124,22 +128,17 @@ def human_to_machine(str):
         res.append("Huonetta '" + room + "' ei ole. Valitse jokin näistä: " + ", ".join(rooms) + ". Käyn muut huoneet läpi.")
   return res
 
-
-def eq3_command_with_extras(str):
-  if in_allday_timer_format(str):
-    return eq3_command(str + ' 24:00')
-  else:
-    return eq3_command(str)
-
 def mi_command(mac):
-  s = ['/home/lowpaw/Downloads/mi_scripts/LYWSD03MMC.py', '--device', mac, '--count', '1']
-  res = subprocess.run(s, stdout=subprocess.PIPE)
-  res_str = res.stdout.decode('utf-8')
+  try:
+    s = ['/home/lowpaw/Downloads/mi_scripts/LYWSD03MMC.py', '--device', mac, '--count', '1']
+    res = subprocess.run(s, stdout=subprocess.PIPE)
+    res_str = res.stdout.decode('utf-8')
+  execpt:
+    res_str = "ERROR: Unknown reading error with a Mi device."
   return res_str
 
 def mi_to_json(mac):
   res = mi_command(mac)
-  print(res)
   mi_json = json.loads("{}")
   try:
     mi_json['temp'] = float(remove_extra_spaces(res.split('Temperature:')[1].split('\nHumidity')[0]))
@@ -151,8 +150,7 @@ def mi_to_json(mac):
     mi_json['battery'] = float('nan')
   return mi_json
   
-def read_mi(s):
-  print('olet oikeassa paikassa!')
+def mi_read_human(s):
   s = remove_extra_spaces(s)
   selected_rooms = s.split(" ")[0].split("-")
   res = []
@@ -163,14 +161,23 @@ def read_mi(s):
     for room in selected_rooms:
       if room in rooms and "mi" in rooms[room]:
         for mi in rooms[room]["mi"]:
-          print('mi huoneessa: ' + room)
           res.append(replace_macs_to_names(mi_command(mi)))
       else:
         res.append("Huonetta '" + room + "' ei ole. Valitse jokin näistä: " + ", ".join(rooms) + ". Käyn muut huoneet läpi.")
   return res
-    
+
+def remove_extra_spaces(str):
+  arr = str.split(" ")
+  i = 0
+  while i < len(arr):
+    if arr[i] == '':
+      arr.pop(i)
+    else:
+      i = i+1
+  return " ".join(arr)
   
 """
+FUNCTIONS THAT ARE NO LONGER USED
 # command : 'mac timer mon 19:00-20:00 22.5'
 # command is identified by 'timer', number of spaces, and '-' between the times
 def in_new_timer_format(command):
@@ -184,18 +191,7 @@ def in_new_timer_format(command):
 def in_allday_timer_format(command):
   arr = command.split(" ")
   return (len(arr) == 4 and arr[1] == 'timer')
-"""
-def remove_extra_spaces(str):
-  arr = str.split(" ")
-  i = 0
-  while i < len(arr):
-    if arr[i] == '':
-      arr.pop(i)
-    else:
-      i = i+1
-  return " ".join(arr)
-  
-"""  
+
 def str_to_time(str):
     if str == '24:00':
         return datetime.strptime('1900-01-02 00:00', '%Y-%m-%d %H:%M')
